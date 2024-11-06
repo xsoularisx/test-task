b2ccac6384c7e8f32c6bc5c41812c2e07b9130bb
albert <xsoularisx@gmail.com>
2024-11-06 18:30:50 +0300

    feat: add modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import './App.css'
 import { useState } from 'react';
-import { Table } from 'antd'
-import { Button } from "antd";
-import { Modal } from "antd";
+import { Input, Button, Modal, Form, Table } from 'antd'
 
 const dataSource = [
   {
@@ -15,6 +13,7 @@ const dataSource = [
     name: 'John',
     age: 42,
   },
+
 ];
 
 const columns = [
@@ -28,19 +27,39 @@ const columns = [
     dataIndex: 'age',
     key: 'age',
   },
+  {
+    title: 'Actions',
+    key: 'actions',
+    render: () => (
+      <>
+        <Button type="default">edit</Button>
+        <Button type="default">delete</Button>
+      </>
+    ),
+  },
 ];
 
 function App() {
 
   const [isModalVisible, setIsModalVisible] = useState(false);
+  const [data, setData] = useState(dataSource);
   const showModal = () => { setIsModalVisible(true); };
   const hideModal = () => { setIsModalVisible(false); };
 
   return (
     <>
-      <Button type="default" className='button' onClick={showModal}>Добавить</Button>
-      <Table dataSource={dataSource} columns={columns} />
-      <Modal title="Добавить запись" open={isModalVisible} onOk={hideModal} onCancel={hideModal}></Modal>
+      <Button type="default" onClick={showModal}>Add</Button>
+      <Table dataSource={data} columns={columns} />
+      <Modal title="Добавить запись" open={isModalVisible} onOk={hideModal} onCancel={hideModal}>
+        <Form>
+          <Form.Item name="fullName" label="Full name" rules={[{ required: true, message: 'Please enter your full name' }]}>
+            <Input />
+          </Form.Item>
+          <Form.Item name="age" label="Age" rules={[{ required: true, message: 'Please enter your age' }]}>
+            <Input type="number" />
+          </Form.Item>
+        </Form>
+      </Modal>
     </>
   )
 }
