65b558f096bbc99db6be17392352e4df9c34017f
albert <xsoularisx@gmail.com>
2024-11-06 22:12:21 +0300

    feat: add linter and prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import './App.css'
+import './App.css';
 import { useState } from 'react';
-import { Input, Button, Modal, Form, Table, Popconfirm } from 'antd'
+import { Input, Button, Modal, Form, Table, Popconfirm } from 'antd';
 
 interface DataRecord {
   key: string;
@@ -9,7 +9,6 @@ interface DataRecord {
 }
 
 function App() {
-
   const dataSource: DataRecord[] = [
     {
       key: '1',
@@ -26,7 +25,6 @@ function App() {
       name: 'Петрова Анна Александровна',
       age: 53,
     },
-
   ];
 
   const columns = [
@@ -35,14 +33,15 @@ function App() {
       dataIndex: 'name',
       key: 'name',
       width: '50%',
-      sorter: (a: { name: string; }, b: { name: any; }) => a.name.localeCompare(b.name),
+      sorter: (a: { name: string }, b: { name: string }) =>
+        a.name.localeCompare(b.name),
     },
     {
       title: 'Age',
       dataIndex: 'age',
       key: 'age',
       width: '25%',
-      sorter: (a: { age: number; }, b: { age: number; }) => a.age - b.age,
+      sorter: (a: { age: number }, b: { age: number }) => a.age - b.age,
     },
     {
       title: 'Actions',
@@ -50,11 +49,20 @@ function App() {
       width: '25%',
       render: (_: string, record: DataRecord) => (
         <>
-          <Button className='button__action' onClick={() => openModal(record)} type="default">edit</Button>
+          <Button
+            className="button__action"
+            onClick={() => openModal(record)}
+            type="default"
+          >
+            edit
+          </Button>
           <Popconfirm
             title="Are you sure you want to delete this record?"
-            onConfirm={() => deleteRecord(record.key)}>
-            <Button className='button__action' type="default">delete</Button>
+            onConfirm={() => deleteRecord(record.key)}
+          >
+            <Button className="button__action" type="default">
+              delete
+            </Button>
           </Popconfirm>
         </>
       ),
@@ -63,7 +71,7 @@ function App() {
 
   const [isModalVisible, setIsModalVisible] = useState(false);
   const [data, setData] = useState<DataRecord[]>(dataSource);
-  const [editData, setEditData] = useState<DataRecord | null>(null)
+  const [editData, setEditData] = useState<DataRecord | null>(null);
   const [form] = Form.useForm();
 
   const openModal = (record: DataRecord | null) => {
@@ -85,10 +93,17 @@ function App() {
   const handleData = async () => {
     try {
       const values = await form.validateFields();
-      const newData: DataRecord = { key: Date.now().toString(), ...values } as DataRecord;
+      const newData: DataRecord = {
+        key: Date.now().toString(),
+        ...values,
+      } as DataRecord;
 
       if (editData) {
-        setData(data.map(item => (item.key === editData.key ? { ...item, ...values } : item)));
+        setData(
+          data.map(item =>
+            item.key === editData.key ? { ...item, ...values } : item,
+          ),
+        );
       } else {
         setData([...data, newData]);
       }
@@ -102,46 +117,54 @@ function App() {
     setData(data.filter(item => item.key !== key));
   };
 
-
   return (
     <>
       <Button
-        className='button__main'
-        type="default" onClick={() => openModal(null)}>Add new record</Button>
-      <Table
-        dataSource={data}
-        columns={columns} />
+        className="button__main"
+        type="default"
+        onClick={() => openModal(null)}
+      >
+        Add new record
+      </Button>
+      <Table dataSource={data} columns={columns} />
       <Modal
         title="Add record"
         open={isModalVisible}
         onOk={handleData}
-        onCancel={closeModal}>
-        <Form
-          form={form}
-          layout="vertical">
+        onCancel={closeModal}
+      >
+        <Form form={form} layout="vertical">
           <Form.Item
             name="name"
             label="Full name"
             rules={[
               { required: true, message: 'Please enter your full name' },
               { min: 2, message: 'Name must contain at least two letters' },
-              { pattern: /^[A-Za-zА-Яа-яЁё\s]+$/, message: 'Name must contain only letters' }
-            ]}>
-            <Input placeholder='Enter your last name, first name, patronymic' />
+              {
+                pattern: /^[A-Za-zА-Яа-яЁё\s]+$/,
+                message: 'Name must contain only letters',
+              },
+            ]}
+          >
+            <Input placeholder="Enter your last name, first name, patronymic" />
           </Form.Item>
           <Form.Item
             name="age"
             label="Age"
             rules={[
               { required: true, message: 'Please enter your age' },
-              { pattern: /^(0|[1-9][0-9]?)$/, message: 'Age must be a non-negative two-digit number (0-99)' },
-            ]}>
-            <Input placeholder='Enter your age' />
+              {
+                pattern: /^(0|[1-9][0-9]?)$/,
+                message: 'Age must be a non-negative two-digit number (0-99)',
+              },
+            ]}
+          >
+            <Input placeholder="Enter your age" />
           </Form.Item>
         </Form>
       </Modal>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
